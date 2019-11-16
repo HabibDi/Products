a8cc33f7b2a16bd510f2877d5522e18a6b859e90
HabibDi <habibdiallo78@gmail.com>
2019-11-16 01:32:57 +0100

    Correction: Fonction like fonctionnelle

--- index.js
+++ index.js
@@ -1,137 +1,113 @@
-let produits = [
+let stock = [
     {
         name: 'PS4',
         price: 350,
         img: "images/logoPS.jpg",
-        like: true
+        liked: false,
     },
 
     {
         name: 'Xbox',
         price: 280,
         img: "images/logoXbox.png",
-        like: false
+        liked: false,
     },
 
     {
         name: 'Switch',
         price: 300,
         img: "images/logoSwitch.png",
-        like: false
+        liked: false,
+    },
 
-    }
 ];
 
-
-// console.log(produits);
-
-var myProducts = document.getElementById('MesProduits')
+var produits = [];
 
 
-produits.forEach((element) => {
+class Produits {
+    constructor(name, price, img, liked) {
+        this.name = name
+        this.price = price
+        this.img = img
+        this.liked = liked
+        produits.push(this)
+    }
 
-    /* (let i = 0; i < produits.length; i++) */
+    like(event) {
 
-    var newProduct = document.createElement('div');
-    var newTitle = document.createElement('h4');
-    var newImage = document.createElement('img');
-    var newInteractLine = document.createElement('div');
-    var newPrice = document.createElement('p');
-    var newBuyButton = document.createElement('button');
-    var newLikeButton = document.createElement('img');
+        if (this.liked === true) {
+            this.liked = false;
+            event.target.setAttribute('src', "images/heartIconClear.png");
+            console.log(this.name + ' unliké')
+        }
 
-    var productName = element.name;
-    var productPic = element.img;
-    var productPrice = element.price;
-    var productLikedStatus = element.like;
+        else if (this.liked === false) {
+            this.liked = true;
+            event.target.setAttribute('src', "images/heartIconFull.png");
+            console.log(this.name + ' liké')
+        }
+        console.log(this.liked)
+    };
+}
 
-    myProducts.appendChild(newProduct);
-    newProduct.appendChild(newTitle);
-    newProduct.appendChild(newImage);
-    newProduct.appendChild(newInteractLine);
-    newInteractLine.appendChild(newPrice);
-    newInteractLine.appendChild(newBuyButton);
-    newInteractLine.appendChild(newLikeButton);
-
-    newProduct.setAttribute('class', 'produit');
-    newProduct.setAttribute('id', productName);
-    newImage.setAttribute('src', productPic);
-    newInteractLine.setAttribute('class', 'interactLine');
-    newLikeButton.setAttribute('src', 'images/heartIconClear.png');
-    newLikeButton.setAttribute('class', 'likeButton')
-    newBuyButton.setAttribute('class', "buyButton")
-
-    newTitle.innerHTML = productName;
-    newPrice.innerHTML = productPrice + '€';
-    newBuyButton.innerHTML = "Acheter";
-    // document.addEventListener('click', addToBasket)
-
-});
-
-var buyB = document.getElementsByClassName("buyButton")
-
-for (let i = 0; i < products.length; i++) {
-
-    buyButton[i].addEventListener("click", function () {
-        alert(event.target.productName + ' ajouté au panier')
-    })
+stock.forEach((item) => {
+    new Produits(item.name, item.price, item.img, item.liked)
+})
 
 
-};
+console.log(produits)
 
+var prodContainer = document.getElementById('MesProduits')
 
-//---------------------------------------------------------
-//             Work in progress down there
-//---------------------------------------------------------
+function build() {
 
+    produits.forEach((element) => {
 
-// function addToBasket(evt) {
-//     alert(evt.currentTarget.productName + ' ajouté au panier');
-//     console.log(productName);
-// };
+        var newProduct = document.createElement('div');
+        var newTitle = document.createElement('h4');
+        var newImage = document.createElement('img');
+        var newInteractLine = document.createElement('div');
+        var newPrice = document.createElement('p');
+        var newBuyButton = document.createElement('button');
+        var newLikeButton = document.createElement('img');
 
-// function like() {
+        var productName = element.name;
+        var productPic = element.img;
+        var productPrice = element.price;
 
-//     if (productLikedStatus === true) {
-//         newLikeButton.removeAttribute('src');
-//         newLikeButton.setAttribute('src', "images/heartIconClear.png");
-//         productLikedStatus = false;
-//         console.log('Produit unliké')
-//     }
+        prodContainer.appendChild(newProduct);
+        newProduct.appendChild(newTitle);
+        newProduct.appendChild(newImage);
+        newProduct.appendChild(newInteractLine);
+        newInteractLine.appendChild(newPrice);
+        newInteractLine.appendChild(newBuyButton);
+        newInteractLine.appendChild(newLikeButton);
 
-//     else if (productLikedStatus === false) {
-//         newLikeButton.removeAttribute('src');
-//         newLikeButton.setAttribute('src', "images/heartIconFull.png");
-//         productLikedStatus = true;
-//         console.log('Produit liké')
-//     }
+        newProduct.setAttribute('class', 'produit');
+        newProduct.setAttribute('id', productName);
+        newImage.setAttribute('src', productPic);
+        newInteractLine.setAttribute('class', 'interactLine');
 
-// };
+        newLikeButton.setAttribute('class', 'likeButton');
+        newLikeButton.setAttribute('src', "images/heartIconClear.png");
 
+        newLikeButton.onclick = function () {
+            element.like(event);
+        }
+
+        newBuyButton.setAttribute('class', "buyButton")
+
+        newTitle.innerHTML = productName;
+        newPrice.innerHTML = productPrice + '€';
+        newBuyButton.innerHTML = "Acheter";
+
+    });
+}
+
+var likeB = document.getElementsByClassName("likeButton");
 
 
 
-// document.addEventListener('click', function (event) {
 
-//     if (event.target.matches('.buyButton')) {
-//         alert(event.target.productName + ' ajouté au panier');
-//     }
-
-// });
-
-// function likeCheck(array) {
-//     for (let i = 0; i < array.length; i++) {
-//         if (array[i].like === true) {
-//             newLikeButton.setAttribute('src', 'images/heartIconFull.png');
-//         } else {
-//             newLikeButton.setAttribute('src', 'images/heartIconClear.png');
-
-//         }
-//     };
-// };
-
-// likeCheck(produits);
-
-
-
-// select->div->boutons 0    select->div->img c
+build()
